ce4b0afcecada01969e32db1d59cc467e24cd147
Asif Mahmood <asiddique@red.sa>
2024-04-21 23:34:23 +0100

    dark mode updates

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
     //       />
     // </header>
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 dark:text-white ">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a href="/" className="flex items-center">
             <img src="/uwe_logo.svg" className="mr-3 h-6 sm:h-9" alt="UWE Logo" />
